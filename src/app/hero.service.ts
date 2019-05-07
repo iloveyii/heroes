50436ce90796033de0d9ef3ab3bc8bf9a50991e2
Hazrat Ali <iloveyii@yahoo.com>
2019-05-07 23:25:24 +0200

    Fix in css

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Hero} from './hero';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
-import {HttpClient, HttpHeaders, httpOptions} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, tap} from 'rxjs/operators';
 
 const httpOptions = {
